4ef510b8de32e3ad4cdefa179331e2f8211fdc70
KylinDC <inlovingdays@gmail.com>
2018-12-23 00:12:25 +0800

    finish practice 8

--- main/practice_8/teacher.js
+++ main/practice_8/teacher.js
@@ -1,1 +1,19 @@
+import { Person } from "./person.js";
 
+class Teacher extends Person {
+  constructor(id, name, age, klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    let basicInfo = `${super.introduce()} I am a Teacher.`;
+    if (this.klass) {
+      return `${basicInfo} I teach ${this.klass.getDisplayName()}.`
+    } else {
+      return `${basicInfo} I teach No Class.`
+    }
+  }
+}
+
+export { Teacher };
