d1524c63c776ca5f8ac381d6e38479e50952e84a
KylinDC <inlovingdays@gmail.com>
2018-12-23 00:12:57 +0800

    finish practice 10

--- main/practice_10/teacher.js
+++ main/practice_10/teacher.js
@@ -1,1 +1,24 @@
+import { Person } from "./person.js";
 
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+
+  introduce() {
+    let basicInfo = `${super.introduce()} I am a Teacher.`;
+    if (this.klasses) {
+      let klassseNumber = this.klasses.map(klass => klass.number);
+      return `${basicInfo} I teach Class ${klassseNumber.join(', ')}.`;
+    } else {
+      return `${basicInfo} I teach No Class.`;
+    }
+  }
+
+  isTeaching(student) {
+    return this.klasses.some(klass => klass.isIn(student));
+  }
+}
+
+export { Teacher };
